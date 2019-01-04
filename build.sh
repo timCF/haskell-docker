20d76821e89bb19b2982ed4dac586e594914fea1
timcf <timcf@sa-84-52-37-115.saturn.infonet.ee>
2019-01-04 16:10:50 +0200

    stack, keter, yesod docker images

--- build.sh
+++ build.sh
@@ -2,4 +2,21 @@
 
 set -e
 
-docker build -t tkachuklabs/ubuntu-16.04-ghc-8.2.2 -f ./Dockerfile-ubuntu-16.04-ghc-8.2.2 .
+TODAY="$(date +%Y-%m-%d)"
+DOCKERHUB_USER="tkachuklabs"
+
+STACK_IMAGE="ubuntu-16.04-stack"
+KETER_IMAGE="ubuntu-16.04-keter-lts-7.19"
+YESOD_IMAGE="ubuntu-16.04-yesod-lts-11.22"
+
+STACK_TAG="$DOCKERHUB_USER/$STACK_IMAGE:$TODAY"
+KETER_TAG="$DOCKERHUB_USER/$KETER_IMAGE:$TODAY"
+YESOD_TAG="$DOCKERHUB_USER/$YESOD_IMAGE:$TODAY"
+
+docker build -t "$STACK_TAG" -f "./Dockerfile-$STACK_IMAGE" .
+docker build -t "$KETER_TAG" -f "./Dockerfile-$KETER_IMAGE" .
+docker build -t "$YESOD_TAG" -f "./Dockerfile-$YESOD_IMAGE" .
+
+echo "$STACK_TAG build succeeded"
+echo "$KETER_TAG build succeeded"
+echo "$YESOD_TAG build succeeded"
--- push.sh
+++ push.sh
@@ -2,4 +2,21 @@
 
 set -e
 
-docker push tkachuklabs/ubuntu-16.04-ghc-8.2.2:latest
+TODAY="$(date +%Y-%m-%d)"
+DOCKERHUB_USER="tkachuklabs"
+
+STACK_IMAGE="ubuntu-16.04-stack"
+KETER_IMAGE="ubuntu-16.04-keter-lts-7.19"
+YESOD_IMAGE="ubuntu-16.04-yesod-lts-11.22"
+
+STACK_TAG="$DOCKERHUB_USER/$STACK_IMAGE:$TODAY"
+KETER_TAG="$DOCKERHUB_USER/$KETER_IMAGE:$TODAY"
+YESOD_TAG="$DOCKERHUB_USER/$YESOD_IMAGE:$TODAY"
+
+docker push "$STACK_TAG"
+docker push "$KETER_TAG"
+docker push "$YESOD_TAG"
+
+echo "$STACK_TAG push succeeded"
+echo "$KETER_TAG push succeeded"
+echo "$YESOD_TAG push succeeded"
